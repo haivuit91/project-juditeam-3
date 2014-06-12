f047ef3972e75ef24b4e19da08bf569cfc79f5d8
haivv.itedu@gmail.com <haivv.itedu@gmail.com@5ad1c160-7e01-7dd7-b96f-7209865af4bb>
2014-06-12 14:08:37 +0000

    [TaiKhoanDAO.java][change]

--- trunk/src/java/model/dao/TaiKhoanDAO.java
+++ trunk/src/java/model/dao/TaiKhoanDAO.java
@@ -9,6 +9,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import model.dao.service.TaiKhoanDAOService;
 import model.entities.TaiKhoan;
@@ -18,7 +19,7 @@ import model.entities.TaiKhoan;
  * @author HAI VU
  */
 public class TaiKhoanDAO implements TaiKhoanDAOService {
-    
+
     private static TaiKhoanDAO tkDAO;
 
     public static TaiKhoanDAO getInstance() {
@@ -30,17 +31,75 @@ public class TaiKhoanDAO implements TaiKhoanDAOService {
 
     @Override
     public List<TaiKhoan> getAllTaiKhoan() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<TaiKhoan> tkList = new ArrayList<>();
+        try {
+            Connection conn = ConnectionFactory.getConnection();
+            String sql = "select * from tbl_taikhoan";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                TaiKhoan tk = new TaiKhoan();
+                tk.setMaTK(rs.getInt("maTK"));
+                tk.setTenTK(rs.getString("tenTK"));
+                tk.setMatkhau(rs.getString("matkhau"));
+                tk.setHoten(rs.getString("hoTen"));
+                tk.setEmail(rs.getString("email"));
+                tk.setDienthoai(rs.getString("dienthoai"));
+                tk.setTrangthai(rs.getInt("trangThai"));
+                tkList.add(tk);
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return tkList;
     }
 
     @Override
     public TaiKhoan getTaiKhoanByMa(int maTK) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        TaiKhoan tk = new TaiKhoan();
+        try {
+            Connection conn = ConnectionFactory.getConnection();
+            String sql = "select * from tbl_taikhoan where maTK = ?";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, maTK);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                tk.setMaTK(rs.getInt("maTK"));
+                tk.setTenTK(rs.getString("tenTK"));
+                tk.setMatkhau(rs.getString("matkhau"));
+                tk.setHoten(rs.getString("hoTen"));
+                tk.setEmail(rs.getString("email"));
+                tk.setDienthoai(rs.getString("dienthoai"));
+                tk.setTrangthai(rs.getInt("trangThai"));
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return tk;
     }
 
     @Override
     public TaiKhoan getTaiKhoanByTenTK(String tenTK) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        TaiKhoan tk = new TaiKhoan();
+        try {
+            Connection conn = ConnectionFactory.getConnection();
+            String sql = "select * from tbl_taikhoan where tenTK = ?";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, tenTK);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                tk.setMaTK(rs.getInt("maTK"));
+                tk.setTenTK(rs.getString("tenTK"));
+                tk.setMatkhau(rs.getString("matkhau"));
+                tk.setHoten(rs.getString("hoTen"));
+                tk.setEmail(rs.getString("email"));
+                tk.setDienthoai(rs.getString("dienthoai"));
+                tk.setTrangthai(rs.getInt("trangThai"));
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return tk;
     }
 
     @Override
