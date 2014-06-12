dc787c6e9d35ced3d2e6a0694d7e70db4e1e9f86
haivv.itedu <haivv.itedu@gmail.com>
2014-06-12 07:34:44 +0000

    [TuLieuDAOService.java][add]

---
+++ src/java/model/dao/service/TuLieuDAOService.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model.dao.service;
+
+import java.util.List;
+import model.entities.TuLieu;
+
+/**
+ *
+ * @author HAI VU
+ */
+public interface TuLieuDAOService {
+    
+    /**
+     * Get AllTuLieu
+     *
+     * @return Tulieu
+     */
+    public List<TuLieu> getAllTuLieu();
+
+    /**
+     * Get TuLieu by maTL
+     * @param maTL is name of user
+     * @return TuLieu
+     */
+    public TuLieu getTuLieuByMaTL(int maTL);
+
+    /**
+     * Get TuLieu by tenTL
+     * @param tenTL
+     * @return TuLieu
+     */
+    public TuLieu getTuLieuByTenTL(String tenTL);
+
+    /**
+     * find TuLieu by tenTL
+     * @param tenTL
+     * @return TuLieu by tenTL
+     */
+    public List<TuLieu> timkiemTuLieuByTenBG(String tenTL);
+    
+    /**
+     * find TuLieu by nam
+     * @param nam
+     * @return TuLieu by nam
+     */
+    public List<TuLieu> timkiemTuLieuByNam(int nam);
+
+    /**
+     * find TuLieu
+     * @param tukhoa
+     * @param dieukien
+     * @return TuLieu
+     */
+    public List<TuLieu> timkiemTuLieu(String tukhoa, String dieukien);
+
+    /**
+     * create TuLieu
+     * @param tulieu
+     * @return TuLieu
+     */
+    public boolean themTuLieu(TuLieu tulieu);
+
+    /**
+     * update TuLieu
+     * @param tulieu
+     * @return TuLieu
+     */
+    public boolean chinhsuaTuLieu(TuLieu tulieu);
+
+    /**
+     * remove TuLieu
+     * @param tulieu
+     * @return TuLieu
+     */
+    public boolean removeTuLieu(TuLieu tulieu);
+
+    /**
+     * restore TuLieu
+     * @param tulieu
+     * @return TuLieu
+     */
+    public boolean restoreTuLieu(TuLieu tulieu);
+
+    /**
+     * delete TuLieu by maTL
+     * @param maTL
+     * @return TuLieu
+     */
+    public boolean xoaTuLieu(int maTL);
+}
