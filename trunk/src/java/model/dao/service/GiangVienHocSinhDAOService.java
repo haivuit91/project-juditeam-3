b27e291f9a7082fc4591f41f217607b12a43b978
haivv.itedu@gmail.com <haivv.itedu@gmail.com@5ad1c160-7e01-7dd7-b96f-7209865af4bb>
2014-06-12 06:24:58 +0000

    [GiangVienHocSinh.java][add][add new DAOService]

--- trunk/src/java/model/dao/service/GiangVienHocSinhDAOService.java
+++ trunk/src/java/model/dao/service/GiangVienHocSinhDAOService.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package model.dao.service;
 
 import java.util.List;
@@ -14,25 +13,84 @@ import model.entities.GiangVienHocSinh;
  * @author HAI VU
  */
 public interface GiangVienHocSinhDAOService {
-    
+
     /**
      * Get GiangVienHocSinh
      *
      * @return GiangVienHocSinh
      */
     public List<GiangVienHocSinh> getAllGiangVienHocSinh();
-    
+
     /**
      * Get GiangVienHocSinh
+     *
      * @param maGVHS
      * @return GiangVienHocSinh
      */
     public List<GiangVienHocSinh> getGiangVienHocSinhByMa(int maGVHS);
-    
+
     /**
      * Get GiangVienHocSinh
-     * @param maGVHS
+     *
+     * @param tenGVHS
      * @return GiangVienHocSinh
      */
-//    public List<GiangVienHocSinh> getGiangVienHocSinhByMa(int maGVHS);
+    public List<GiangVienHocSinh> getGiangVienHocSinhByTen(int tenGVHS);
+
+    /**
+     * find GiangVienDeCuong
+     *
+     * @param tukhoa
+     * @param dieukien
+     * @return GiangVienDeCuong
+     */
+    public List<GiangVienHocSinh> timkiemGiangVienHocSinh(String tukhoa, String dieukien);
+
+    /**
+     * find GiangVienDeCuong
+     *
+     * @param tenGVHS
+     * @return GiangVienDeCuong
+     */
+    public List<GiangVienHocSinh> timkiemGiangVienHocSinhByTen(String tenGVHS);
+
+    /**
+     * create GiangVienHocSinh
+     *
+     * @param gvhs
+     * @return GiangVienHocSinh
+     */
+    public boolean themGiangVienHocSinh(GiangVienHocSinh gvhs);
+
+    /**
+     * update GiangVienHocSinh
+     *
+     * @param gvhs
+     * @return GiangVienHocSinh
+     */
+    public boolean chinhsuaGiangVienHocSinh(GiangVienHocSinh gvhs);
+
+    /**
+     * remove GiangVienHocSinh
+     *
+     * @param gvhs
+     * @return GiangVienHocSinh
+     */
+    public boolean removeGiangVienHocSinh(GiangVienHocSinh gvhs);
+
+    /**
+     * restore GiangVienHocSinh
+     *
+     * @param gvhs
+     * @return GiangVienHocSinh
+     */
+    public boolean restoreGiangVienHocSinh(GiangVienHocSinh gvhs);
+
+    /**
+     * delete GiangVienHocSinh
+     *
+     * @param gvhs
+     * @return GiangVienHocSinh
+     */
+    public boolean xoaGiangVienHocSinh(GiangVienHocSinh gvhs);
 }
