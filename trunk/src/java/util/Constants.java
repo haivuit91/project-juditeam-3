e88e1b81af4c7837e28c5d9d84b8d9bdd33584fd
haivv.itedu@gmail.com <haivv.itedu@gmail.com@5ad1c160-7e01-7dd7-b96f-7209865af4bb>
2014-06-12 23:45:11 +0000

    [Constans.java][change]

--- trunk/src/java/util/Constants.java
+++ trunk/src/java/util/Constants.java
@@ -45,4 +45,6 @@ public class Constants {
     public static final String CURRENT_USER = "currentUser";
     
     public static final String GVHS_LIST = "gvhsList";
+    
+    public static final String TL_LIST = "tulieuList";
 }
