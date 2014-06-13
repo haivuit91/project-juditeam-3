c7cbbdf94db6377c653e99d1b504f066355a8f9f
tuyenbq.itedu <tuyenbq.itedu@gmail.com>
2014-06-13 03:12:03 +0000

    [Constants.java][update][update]

--- src/java/util/Constants.java
+++ src/java/util/Constants.java
@@ -46,5 +46,7 @@ public class Constants {
     
     public static final String GVHS_LIST = "gvhsList";
     
+    public static final String BG_LIST = "bgList";
+    
     public static final String TL_LIST = "tulieuList";
 }
