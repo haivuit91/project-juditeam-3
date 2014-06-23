be2a414f98ce7b1f329f90ad0acbb4c2c22ce609
haivv.itedu <haivv.itedu@gmail.com>
2014-06-23 04:55:37 +0000

    [change]

--- src/java/util/Support.java
+++ src/java/util/Support.java
@@ -8,7 +8,9 @@ package util;
 import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Properties;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.mail.Message;
@@ -24,6 +26,33 @@ import javax.mail.internet.MimeMessage;
  * @author Admin
  */
 public class Support {
+
+    private static char[] SPECIAL_CHARACTERS = {' ', '!', '"', '#', '$', '%',
+        '*', '+', ',', ':', '<', '=', '>', '?', '@', '[', '\\', ']', '^',
+        '`', '|', '~', 'À', 'Á', 'Â', 'Ã', 'È', 'É', 'Ê', 'Ì', 'Í', 'Ò',
+        'Ó', 'Ô', 'Õ', 'Ù', 'Ú', 'Ý', 'à', 'á', 'â', 'ã', 'è', 'é', 'ê',
+        'ì', 'í', 'ò', 'ó', 'ô', 'õ', 'ù', 'ú', 'ý', 'Ă', 'ă', 'Đ', 'đ',
+        'Ĩ', 'ĩ', 'Ũ', 'ũ', 'Ơ', 'ơ', 'Ư', 'ư', 'Ạ', 'ạ', 'Ả', 'ả', 'Ấ',
+        'ấ', 'Ầ', 'ầ', 'Ẩ', 'ẩ', 'Ẫ', 'ẫ', 'Ậ', 'ậ', 'Ắ', 'ắ', 'Ằ', 'ằ',
+        'Ẳ', 'ẳ', 'Ẵ', 'ẵ', 'Ặ', 'ặ', 'Ẹ', 'ẹ', 'Ẻ', 'ẻ', 'Ẽ', 'ẽ', 'Ế',
+        'ế', 'Ề', 'ề', 'Ể', 'ể', 'Ễ', 'ễ', 'Ệ', 'ệ', 'Ỉ', 'ỉ', 'Ị', 'ị',
+        'Ọ', 'ọ', 'Ỏ', 'ỏ', 'Ố', 'ố', 'Ồ', 'ồ', 'Ổ', 'ổ', 'Ỗ', 'ỗ', 'Ộ',
+        'ộ', 'Ớ', 'ớ', 'Ờ', 'ờ', 'Ở', 'ở', 'Ỡ', 'ỡ', 'Ợ', 'ợ', 'Ụ', 'ụ',
+        'Ủ', 'ủ', 'Ứ', 'ứ', 'Ừ', 'ừ', 'Ử', 'ử', 'Ữ', 'ữ', 'Ự', 'ự',};
+
+    private static char[] REPLACEMENTS = {'-', '\0', '\0', '\0', '\0', '\0',
+        '\0', '_', '\0', '_', '\0', '\0', '\0', '\0', '\0', '\0', '_',
+        '\0', '\0', '\0', '\0', '\0', 'A', 'A', 'A', 'A', 'E', 'E', 'E',
+        'I', 'I', 'O', 'O', 'O', 'O', 'U', 'U', 'Y', 'a', 'a', 'a', 'a',
+        'e', 'e', 'e', 'i', 'i', 'o', 'o', 'o', 'o', 'u', 'u', 'y', 'A',
+        'a', 'D', 'd', 'I', 'i', 'U', 'u', 'O', 'o', 'U', 'u', 'A', 'a',
+        'A', 'a', 'A', 'a', 'A', 'a', 'A', 'a', 'A', 'a', 'A', 'a', 'A',
+        'a', 'A', 'a', 'A', 'a', 'A', 'a', 'A', 'a', 'E', 'e', 'E', 'e',
+        'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'I',
+        'i', 'I', 'i', 'O', 'o', 'O', 'o', 'O', 'o', 'O', 'o', 'O', 'o',
+        'O', 'o', 'O', 'o', 'O', 'o', 'O', 'o', 'O', 'o', 'O', 'o', 'O',
+        'o', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u',
+        'U', 'u',};
 
     public static boolean sendFeedBackMail(String email, String fullName, String title, String content) {
 
@@ -64,9 +93,11 @@ public class Support {
         }
         return isCheck;
     }
+
     /**
      * Convert three args day month year to type java.sql.Date
-     * @param day 
+     *
+     * @param day
      * @param month
      * @param year
      * @return Date type: yyyy/MM/dd
@@ -82,5 +113,55 @@ public class Support {
             Logger.getLogger(Support.class.getName()).log(Level.SEVERE, null, ex);
         }
         return date;
+    }
+
+    public static String randomString() {
+        int size = 8;
+        String str01 = "abcdefghijklmnopqrstuvwxyz";
+        String str02 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String str03 = "0123456789";
+        String strValid = str01 + str02 + str03;
+        Random random = new Random();
+        String mystring = "";
+        for (int i = 0; i < size; i++) {
+            int randnum = random.nextInt(strValid.length());
+            mystring = mystring + strValid.charAt(randnum);
+        }
+        return mystring;
+    }
+
+    public static String toUrlFriendly(String s) {
+        int maxLength = Math.min(s.length(), 236);
+        char[] buffer = new char[maxLength];
+        int n = 0;
+        for (int i = 0; i < maxLength; i++) {
+            char ch = s.charAt(i);
+            buffer[n] = removeAccent(ch);
+            // skip not printable characters
+            if (buffer[n] > 31) {
+                n++;
+            }
+        }
+        // skip trailing slashes
+        while (n > 0 && buffer[n - 1] == '/') {
+            n--;
+        }
+        return String.valueOf(buffer, 0, n);
+    }
+
+    public static char removeAccent(char ch) {
+        int index = Arrays.binarySearch(SPECIAL_CHARACTERS, ch);
+        if (index >= 0) {
+            ch = REPLACEMENTS[index];
+        }
+        return ch;
+    }
+
+    public static String removeAccent(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        for (int i = 0; i < sb.length(); i++) {
+            sb.setCharAt(i, removeAccent(sb.charAt(i)));
+        }
+        return sb.toString();
     }
 }
