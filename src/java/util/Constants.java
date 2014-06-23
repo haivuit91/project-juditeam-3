882a150d04c02ee923217ecc3042104223d9b706
haivv.itedu <haivv.itedu@gmail.com>
2014-06-23 02:37:23 +0000

    [change]

--- src/java/util/Constants.java
+++ src/java/util/Constants.java
@@ -56,7 +56,7 @@ public class Constants {
     
     public static final String RESULT_UPLOAD = "result_upload";
     
-    public static final String URL_IMAGE = "url_image";
+    public static final String URL_FILE = "url_file";
     
     public static final String STORAGE_PATH = "project3/config/";
     
